748d3bb31616d534ac0c602aca8dcb9f20d7b8d9
Florencia Lafuente <florencia.lafuente@okta.com>
2024-10-16 11:23:50 -0300

    add create jwk

--- pages/api/createJWK.js
+++ pages/api/createJWK.js
@@ -1,8 +1,21 @@
-const oktaDomain = process.env.OKTA_DOMAIN;
+const fs = require('fs');
+const jose = require('node-jose');
+
 
 const handler = async (req, res) => {
-  
-  res.send({})
+  const keyStore = await jose.JWK.createKeyStore()
+  keyStore.generate('RSA', 2048, {alg: 'RS256', use: 'sig' })
+  .then(result => {
+    fs.writeFileSync(
+      'privateKey.json', 
+      JSON.stringify(keyStore.toJSON(true), null, '  ')
+    )
+    fs.writeFileSync(
+      'publicKey.json', 
+      JSON.stringify(keyStore.toJSON(), null, '  ')
+    )
+  })
+  res.send({ message: 'Key generated' })
 
 }
 
